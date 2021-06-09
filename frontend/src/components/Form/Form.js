03fba4c51385e7ca6a39256897305fed9c7b6e0e
fuadashraful <fuadashraful16@gmail.com>
2021-06-09 10:00:55 +0600

    mern blog project created

---
+++ frontend/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import useStyles from './styles';
+const Form=()=>{
+    return(
+        <div>
+            <h1>Form</h1>
+        </div>
+    )
+}
+
+export default Form;
